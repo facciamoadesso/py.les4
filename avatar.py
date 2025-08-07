ea510beee2317147b99cc3dc222dc8e71ae17bc4
facciamoadesso <shakaraizada@gmail.com>
2025-08-07 12:48:50 +0200

    Delete avatar.py

--- avatar.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-image = Image.open("merged_channels.jpg")
-print("Original size:", image.size)
-image.thumbnail((80, 80))
-print("Thumbnail size:", image.size)
-image.save("avatar.jpg")
-print("avatar.jpg")
