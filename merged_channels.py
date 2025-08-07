dc145619be116b0a9873d42f6cf0c8348cf13792
facciamoadesso <shakaraizada@gmail.com>
2025-08-07 12:50:00 +0200

    Delete merged_channels.py

--- merged_channels.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-red = Image.open("shifted_red.jpg")
-green = Image.open("cropped_green.jpg")
-blue = Image.open("shifted_blue.jpg")
-merged = Image.merge("RGB", (red, green, blue))
-merged.save("merged_channels.jpg")
-print("merged_channels.jpg")
