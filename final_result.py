fb1c025c9ba8594eea03a1890ab2c794acddebae
facciamoadesso <shakaraizada@gmail.com>
2025-08-17 22:39:04 +0500

    Высушенный код с исправленным алгоритмом

--- final_result.py
+++ final_result.py
@@ -1,33 +1,20 @@
 from PIL import Image
 
-image = Image.open("monro.jpg")
-rgb_image = image.convert("RGB")
-red, green, blue = rgb_image.split()
-
-merged_image = Image.merge("RGB", (red, green, blue))
-
 red = Image.open("red_channel.jpg")
 red_left = red.crop((50, 0, red.width, red.height))
 red_middle = red.crop((25, 0, red.width - 25, red.height))
-blended_red = Image.blend(red_left, red_middle, 0.5)
-final_red = blended_red.crop((0, 0, 595, 522))
+blended_red = Image.blend(red_left, red_middle, 0.5).crop((0, 0, 595, 522))
 
 blue = Image.open("blue_channel.jpg")
-blue_right = blue.crop((50, 0, blue.width, blue.height))
+blue_right = blue.crop((0, 0, blue.width-50, blue.height))
 blue_middle = blue.crop((25, 0, blue.width - 25, blue.height))
-blended_blue = Image.blend(blue_right, blue_middle, 0.5)
-final_blue = blended_blue.crop((0, 0, 595, 522))
+blended_blue = Image.blend(blue_right, blue_middle, 0.5).crop((0, 0, 595, 522))
 
-green = Image.open("green_channel.jpg")
-cropped_green = green.crop((25, 0, 620, 522))
+green = Image.open("green_channel.jpg").crop((25, 0, 620, 522))
 
-red = Image.open("shifted_red.jpg")
-green = Image.open("cropped_green.jpg")
-blue = Image.open("shifted_blue.jpg")
-final_merged = Image.merge("RGB", (red, green, blue))
+final_merged = Image.merge("RGB", (blended_red, green, blended_blue))
 final_merged.save("merged_channels.jpg")
 
 avatar = Image.open("merged_channels.jpg")
 avatar.thumbnail((80, 80))
-
 avatar.save("avatar.jpg")
